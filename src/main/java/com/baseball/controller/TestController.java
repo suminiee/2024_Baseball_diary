b251311e5814e05bef3e84be93efd3f3a83f8d5e
suminiee <chansu0114@naver.com>
2024-05-30 21:32:09 +0900

    Feat: 로그인 마크업 & JQuery 완성

--- src/main/java/com/baseball/controller/TestController.java
+++ src/main/java/com/baseball/controller/TestController.java
@@ -5,14 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
-    @GetMapping("/")
-    public String index() {
-        return "beforeLogin";
-    }
 
     @GetMapping("/main")
     public String main() {
-        return "afterLogin";
+        return "main";
     }
 
     @GetMapping("/signUp")
