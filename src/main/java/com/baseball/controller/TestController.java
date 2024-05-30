8c9fbfcf9ad0627d7c1ea6216ec6c393c03761b2
suminiee <chansu0114@naver.com>
2024-05-30 21:32:36 +0900

    Feat: 응원 팀 변경 마크업

--- src/main/java/com/baseball/controller/TestController.java
+++ src/main/java/com/baseball/controller/TestController.java
@@ -6,18 +6,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-    @GetMapping("/main")
+    //로그인 전 메인페이지
+    @GetMapping("/")
     public String main() {
         return "main";
     }
 
+    //로그인 후 메인페이지
+    @GetMapping("/main")
+    public String loginMain() {
+        return "loginMain";
+    }
+
+    //회원가입 페이지
     @GetMapping("/signUp")
     public String signUp() {
         return "signUp";
     }
 
+    //로그인 페이지
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    //응원하는 팀 변경페이지
+    @GetMapping("/changeMyTeam")
+    public String changeMyTeam() {
+        return "changeMyTeam";
     }
 }
